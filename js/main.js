f9f93e11bb6173cedd5e8cbce47711e18bc611c7
YinkaAri14 <aariwa2@lsu.edu>
2024-12-03 12:06:32 -0600

    Testing of Carousel On new site

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 // JS scripts placed here
+$(document).ready(function(){
+    $('.rending-carousel').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+         cssEase: 'linear'
+    });
+  });
