9913264ac58020603d1d6ff38976673f0fcde76d
YinkaAri14 <aariwa2@lsu.edu>
2024-12-04 17:10:30 -0600

    changing the images and adjust the carousel buttons

---
+++ js/about.js
@@ -0,0 +1,11 @@
+// JS scripts placed here
+$(document).ready(function(){
+    $('.about-carousel').slick({
+        autoplay: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+         cssEase: 'linear'
+    });
+  });
